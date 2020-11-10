234509767d9a303d819971267d5d7245b5e73bbe
developern1 <developern.com@gmail.com>
2020-11-10 13:07:24 +0900

    06-4/액션04

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
@@ -15,7 +15,6 @@ function Movie({id, year, title, summary, poster }) {
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
