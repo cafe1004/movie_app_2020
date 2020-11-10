3935318b87feb8685a84953b1d0c119006428568
developern1 <developern.com@gmail.com>
2020-11-10 16:25:58 +0900

    08-4/액션05

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { Link } from 'react-router-dom';
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
+      <Link
+        to={{
+          pathname: '/movie-detail',
+          state: { year, title, summary, poster, genres }
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
         <div className="movie-data">
           <h3 className="movie-title">{title}</h3>
           <h5 className="movie-year">{year}</h5>
@@ -16,6 +23,7 @@ function Movie({ year, title, summary, poster, genres }) {
           </ul>
           <p className="movie-summary">{summary.slice(0, 180)}...</p>
         </div>
+      </Link>
     </div>
   ); 
 }
